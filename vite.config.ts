834ab30c944708b7d1eb448acf612e7a515c8c27
Julius Henke <mail@juliushenke.com>
2022-04-29 16:07:57 +0200

    fix: disable typescript lib checking

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import viteChecker from 'vite-plugin-checker'
+import viteReact from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
+        viteReact(),
+        viteChecker({ typescript: true }),
     ],
 })
